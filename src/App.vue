03aaf524ba03656f0d82a94e5f538e797662080d
Rohmat Sidiq M <rohmatsidiq9@gmail.com>
2025-05-15 15:02:01 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import {ref} from 'vue'
+const count = ref(0)
+</script>
+
+<template>
+  <main>
+    <h1>Conter</h1>
+    <span id="value">{{ count }}</span>
+    <div>
+      <button @click="count--" id="decrease">-</button>
+      <button @click="count++" id="increase">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+main {
+  text-align: center;
+}
+
+#value {
+  font-size: 50px
+}
+
+</style>
