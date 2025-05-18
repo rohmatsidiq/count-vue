f020e865a0166c44572a98d3268ce57a2fbcd3fd
Khansa <rohmatsidiq9@gmail.com>
2025-05-18 19:48:40 +0700

    composition api and options api

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,41 @@
-<script setup>
-import {ref} from 'vue'
-const count = ref(0)
+<!-- <script setup> -->
+<script>
+import { ref } from "vue";
+
+export default {
+  // options api =========================
+  // buat variable
+  // data() {
+  //   return {
+  //     count: 0,
+  //   };
+  // },
+  // // buat fungsi
+  // methods: {
+  //   increase() {
+  //     this.count++;
+  //   },
+  //   decrease() {
+  //     this.count--;
+  //   },
+  // },
+
+  // compositon api ==========================
+  setup() {
+    const count = ref(0);
+    function increase() {
+      count.value++;
+    }
+    function decrease() {
+      count.value--;
+    }
+    return {
+      count,
+      increase,
+      decrease,
+    };
+  },
+};
 </script>
 
 <template>
@@ -8,20 +43,18 @@ const count = ref(0)
     <h1>Conter</h1>
     <span id="value">{{ count }}</span>
     <div>
-      <button @click="count--" id="decrease">-</button>
-      <button @click="count++" id="increase">+</button>
+      <button @click="decrease" id="decrease">-</button>
+      <button @click="increase" id="increase">+</button>
     </div>
   </main>
 </template>
 
 <style scoped>
-
 main {
   text-align: center;
 }
 
 #value {
-  font-size: 50px
+  font-size: 50px;
 }
-
 </style>
